1c1e2d70e9af42eef468becdad5b1bb883bcb633
Onyeabor Joel <earlmorningstar@gmail.com>
2023-09-26 20:26:21 +0100

    Countdown Timer Commit.

---
+++ Countdown-Timer/app.js
@@ -0,0 +1,30 @@
+const daysPole = document.getElementById("days");
+const hoursPole = document.getElementById("hours");
+const minsPole = document.getElementById("mins");
+const secPole = document.getElementById("sec");
+
+const newYear = "1 jan 2024";
+
+function countdown() {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const sec = Math.floor(totalSeconds) % 60;
+
+  // console.log(newYearDate - currentDate);
+  // console.log(days, hours, mins, seconds);
+}
+
+daysPole.innerHTML = days;
+hoursPole.innerHTML = hours;
+minsPole.innerHTML = mins;
+secPole.innerHTML = sec;
+
+countdown();
+
+setInterval(countdown, 1000);
